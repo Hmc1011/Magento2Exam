a2e6f3378ecc81e52b341952df724a61ce49cff7
hmc1011 <hmc10111994@gmail.com>
2021-03-28 18:59:46 +0700

    cached by Redis,used memcached to save sessions and enable opcache

--- app/etc/env_example.php
+++ app/etc/env_example.php
@@ -37,20 +37,51 @@ return [
     ],
     'x-frame-options' => 'SAMEORIGIN',
     'MAGE_MODE' => 'developer',
-    'session' => [
-        'save' => 'files'
-    ],
-    'cache' => [
-        'frontend' => [
-            'default' => [
-                'id_prefix' => 'f73_'
-            ],
-            'page_cache' => [
-                'id_prefix' => 'f73_'
-            ]
-        ],
-        'allow_parallel_generation' => false
-    ],
+    'session' =>array(
+'save' => 'memcached',
+'save_path' => '127.0.0.1:11211?persistent=1&weight=2&timeout=10&retry_interval=10',
+)
+    
+    ,
+    'cache' => array (
+'frontend' => array (
+'default' => array (
+'backend' => 'Cm_Cache_Backend_Redis',
+'backend_options' => array (
+'server' => '127.0.0.1',
+'port' => '6379',
+'persistent' => '',
+'database' => '0',
+'force_standalone' => '0',
+'connect_retries' => '1',
+'read_timeout' => '10',
+'automatic_cleaning_factor' => '0',
+'compress_data' => '1',
+'compress_tags' => '1',
+'compress_threshold' => '20480',
+'compression_lib' => 'gzip',
+),
+),
+'page_cache' => array (
+'backend' => 'Cm_Cache_Backend_Redis',
+'backend_options' => array (
+'server' => '127.0.0.1',
+'port' => '6379',
+'persistent' => '',
+'database' => '1',
+'force_standalone' => '0',
+'connect_retries' => '1',
+'read_timeout' => '10',
+'automatic_cleaning_factor' => '0',
+'compress_data' => '0',
+'compress_tags' => '1',
+'compress_threshold' => '20480',
+'compression_lib' => 'gzip',
+),
+),
+),
+)
+    ,
     'lock' => [
         'provider' => 'db',
         'config' => [
